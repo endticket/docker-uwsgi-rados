58e3b8811e77b7a2de743068a560ba7c389eba11
pas <pas@zomg.hu>
2016-09-26 17:53:46 +0200

    leanr containrrr

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ wget https://download.ceph.com/keys/release.asc -O - | apt-key add -
 
 echo 'deb https://download.ceph.com/debian-jewel xenial main' > /etc/apt/sources.list.d/ceph.list
 
-apt-get update
+apt-get                            update
 apt-get -y                         upgrade
-apt-get -y --no-install-recommends install unzip librados-dev ceph-common curl make gcc
+apt-get -y --no-install-recommends install unzip librados2 librados-dev ceph-common curl make gcc
 # curl http://uwsgi.it/install | bash -s rados /usr/local/bin/uwsgi
 
 
@@ -21,6 +21,10 @@ unzip uwsgi-master.zip
 
 cd uwsgi-master
 UWSGI_PROFILE=rados UWSGI_BIN_NAME=/usr/local/bin/uwsgi make
+
+
+apt-get -y purge librados-dev make gcc unzip
+apt-get -y autoremove
 
 rm -rf /var/lib/apt/lists/*
 cd /
